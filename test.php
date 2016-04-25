673df653bbd96df7b2ecea1db0cb6981ac9c70d2
lujiang <531432594@qq.com>
2016-04-25 18:11:17 +0800

    修改test.php
    修改test.php

--- test.php
+++ test.php
@@ -1,13 +1,4 @@
 <?php
-$arr = array('openid'=>1);
-$str = 'openidddd';
-
-echo 'start';
-if( !empty($str['openid']) ){
-    echo 'str true';
-}
-echo "\n";
-
-if( isset($str['openid']) ){
-    echo 'isset true';
-}
+/**
+ *
+ */
